c6c1a10ebb7a792b6165c356d76e3a90bb20a9f3
Asjad <asjadbutt06@gmail.com>
2023-02-10 17:44:27 +0500

    sync

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router} from '@angular/router';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { RoleGuard } from 'src/guards/role.guard';
 import { User } from 'src/Models/user';
+import { UserForLogin } from 'src/Models/userforlogin';
 import { AccountService } from 'src/Services/account.service';
 
 
@@ -15,7 +16,7 @@ import { AccountService } from 'src/Services/account.service';
 export class WelcomeComponent implements OnInit {
   loginMode = false;
   registerMode = false;
-  user!: User;
+  user!: UserForLogin;
   errorMessage!: string;
 
   constructor(private accountsService: AccountService, private roleGuard: RoleGuard,
@@ -23,9 +24,15 @@ export class WelcomeComponent implements OnInit {
   model: any = {};
 
   ngOnInit(): void {
+    localStorage.setItem('user', 'hello');
   }
   onLogin() {
-    this.accountsService.login(this.model).subscribe(()=>{
+    this.accountsService.login(this.model).subscribe((res)=>{
+      if (window.localStorage) {
+        console.log('local storage supported !!!');
+      } else {
+        console.log('local storage not supported  $$$ ');
+      }
     });
   }
 
